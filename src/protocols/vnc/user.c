ba0c66a543104e5f49846e66407d42d26b9b9685
itsankoff <ivaylotsankov93@gmail.com>
2017-10-24 16:47:15 +0300

    GUACAMOLE-424: Fix a crash if vnc password does not match remote password

--- src/protocols/vnc/user.c
+++ src/protocols/vnc/user.c
@@ -112,6 +112,10 @@ int guac_vnc_user_leave_handler(guac_user* user) {
 
     guac_vnc_client* vnc_client = (guac_vnc_client*) user->client->data;
 
+    if (vnc_client->display == NULL) {
+        return 1;
+    }
+
     /* Update shared cursor state */
     guac_common_cursor_remove_user(vnc_client->display->cursor, user);
 
